da28c2fbe91c9cea8890028e8d24b802dc40661f
alexklenio <alexlopes.see@gmail.com>
2025-09-21 16:16:31 -0300

    exercises

---
+++ tipos_variavies/condicionais.py
@@ -0,0 +1,25 @@
+#id, elif, else
+
+idade = 19
+
+
+if idade >= 18:
+    print("VOcê é maior de idade.")
+elif idade >= 12:
+    print("Você é um adolescente")
+else:
+    print("Você é maior de idade")
+
+
+if idade == 19:
+    print("Você tem 19 anos")
+
+if idade < 18:
+    print("Você é menor de idade")
+
+if idade != 10:
+    print("Você não tem 10 anos!")
+
+
+mensagem = "maior de idade"if idade >= 18 else "Menor de idade"
+print(mensagem)
--- tipos_variavies/dixionarios.py
+++ tipos_variavies/dixionarios.py
@@ -21,3 +21,22 @@ print("Idade atualizada: ", pessoa["idade"])
 del pessoa["sobrenome"]
 print("Meu dicionário de exemplo: ", pessoa)
 
+#metodo: keys()
+chaves = pessoa.keys()
+print("chaves do dicionário: ", chaves)
+
+chaves = list(pessoa.keys())
+print("Primera chave: ", chaves[0])
+
+#metodo: values()
+valores = pessoa.values()
+print("Valores do dicionário: ", valores)
+
+valores = list(pessoa.values())
+print("Primeiro valor do dicionário:", valores[0])
+
+#motodo: itens()
+
+itens = list(pessoa.items())
+print("Pares chave-valor do dicionário: ", valores)
+print(f"Primeiro valor: {itens[0][0]} = {itens[0][1]}")
