5f0c51cf0fd9bdc954c75c809d1720a78961e49e
alexklenio <alexlopes.see@gmail.com>
2025-09-20 23:30:42 -0300

    Create listas.py

---
+++ tipos_variavies/listas.py
@@ -0,0 +1,15 @@
+#declaração
+
+minha_lista = [1, 2, 3, 4, 5, "Alex", True, False]
+
+#exibindo a lista
+print("Minha Lista de Exemplo", minha_lista)
+
+#exibindi os elementos
+print("Minha lista[0]:", minha_lista[0])
+print("Minha lista[5]:", minha_lista[5])
+
+#fatiamento
+print("Minha lista[1:7]:", minha_lista[1:7])
+print("Minha lista[:6]:", minha_lista[:6])
+print("Minha lista[2:]:", minha_lista[2:])
