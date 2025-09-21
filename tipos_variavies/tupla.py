60bbc0762a52c913e8fa70a7f6d2d970c5ca22c8
alexklenio <alexlopes.see@gmail.com>
2025-09-20 23:51:45 -0300

    Create tupla.py

---
+++ tipos_variavies/tupla.py
@@ -0,0 +1,7 @@
+minha_tupla = (1, 2, 3, 4, 5)
+print("Minha tupla: ", minha_tupla)
+
+print("minha_tupla[0]: ", minha_tupla[0])
+
+contagem = minha_tupla.count(2)
+print("QUantidade de vezes que o elemento 2 aparece: ", contagem)
