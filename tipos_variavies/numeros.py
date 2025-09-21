b6ca1459d764c68c012e50ed11a0637ea6a2c7ac
alexklenio <alexlopes.see@gmail.com>
2025-09-20 22:39:51 -0300

    Update numeros.py

--- tipos_variavies/numeros.py
+++ tipos_variavies/numeros.py
@@ -9,3 +9,21 @@ print("Real componto flutuante = ", numero_real)
 # type()
 print("TIpo davariável inteiro = ", type(numero_inteiro))
 print("TIpo davariável real = ", type(numero_real))
+
+soma = 1 + 1
+print("1 + 1 = ", soma)
+
+subtração = 1 - 1
+print("1 + 1 = ", subtração)
+
+multiplicação = 2 * 2
+print("2 x 2 = ", multiplicação)
+
+divisao = 5 / 2
+print("5 / 2 = ", divisao)
+
+modulo = 5 % 2
+print("Modulo = ", modulo)
+
+divisao_inteira = 5 // 2
+print("5 // 2 = ", divisao)
