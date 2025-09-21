8e76b1bbd32aaf8137d5b0231276f64b571323c7
alexklenio <alexlopes.see@gmail.com>
2025-09-20 22:31:04 -0300

    Update numeros.py

--- tipos_variavies/numeros.py
+++ tipos_variavies/numeros.py
@@ -5,3 +5,7 @@ print("Inteiro = ", numero_inteiro)
 # Real com ponto flutuante
 numero_real = 3.14
 print("Real componto flutuante = ", numero_real)
+
+# type()
+print("TIpo davariável inteiro = ", type(numero_inteiro))
+print("TIpo davariável real = ", type(numero_real))
