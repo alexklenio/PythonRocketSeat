7048062b7331d3b2b8b47e3e9ca9bbdcb8381eda
alexklenio <alexlopes.see@gmail.com>
2025-09-22 20:06:57 -0300

    Create 06 - decoradores.py

---
+++ 02 - programacao_orientada_a_objetos/06 - decoradores.py
@@ -0,0 +1,13 @@
+def meu_decorador(func):
+    def wrapper():
+        print("Antes da função ser chamada")
+        func()
+        print("Depois da função ser chamada")
+    return wrapper
+
+@meu_decorador
+def minha_funcao():
+    print("Minha função foi chamada")
+
+
+minha_funcao()
