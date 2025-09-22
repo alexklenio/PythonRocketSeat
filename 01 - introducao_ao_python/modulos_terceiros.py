4605c9c1a58f65b285347e6bedb1d444be478e18
alexklenio <alexlopes.see@gmail.com>
2025-09-22 18:41:48 -0300

    organization

---
+++ 01 - introducao_ao_python/modulos_terceiros.py
@@ -0,0 +1,9 @@
+#pip3 install requests==2.31.0
+
+print("\nImportação e uso de um módulo de terceiros")
+
+import requests
+
+url = "htts://example.com"
+response = requests.get(url)
+print(f"Solicitação HTTP para {url} retornou o status {response.status_code}")
--- modulos_terceiros.py
+++
@@ -1,9 +0,0 @@
-#pip3 install requests==2.31.0
-
-print("\nImportação e uso de um módulo de terceiros")
-
-import requests
-
-url = "htts://example.com"
-response = requests.get(url)
-print(f"Solicitação HTTP para {url} retornou o status {response.status_code}")
