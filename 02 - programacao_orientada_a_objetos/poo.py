838f1d0b59e28f7e67f8e09edfaba797791fd7f8
alexklenio <alexlopes.see@gmail.com>
2025-09-22 19:06:34 -0300

    Update poo.py

---
+++ 02 - programacao_orientada_a_objetos/poo.py
@@ -0,0 +1,19 @@
+
+#classe exemplo
+class Pessoa:
+    def __init__(self, nome, idade) -> None:
+        self.nome = nome
+        self.idade = idade
+
+    def saudacao(self):
+        return f"Olá,meu nome é {self.nome} e eu tenho {self.idade} anos."
+
+
+# Objetos
+pessoa1 = Pessoa("Alex", 39)
+mensagem = pessoa1.saudacao()
+print(mensagem)
+
+pessoa2 = Pessoa("Kaionara", 28)
+mensagem = pessoa2.saudacao()
+print(mensagem)
